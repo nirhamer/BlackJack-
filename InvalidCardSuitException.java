4b2c4a9af5031d4f5fa4d593cdb876344d5096b6
nir <nirhamer17@walla.co.il>
2021-05-24 19:10:25 +0300

    playable black jack game initial commit to be updated

---
+++ InvalidCardSuitException.java
@@ -0,0 +1,26 @@
+public class InvalidCardSuitException extends Exception {
+
+	private char suitIdentifier = '?';
+
+	public InvalidCardSuitException (char invalidSuit) {
+
+		suitIdentifier = invalidSuit;
+
+		System.out.println("Invalid suit" + " " + invalidSuit);
+	}
+
+	private InvalidCardSuitException() {
+		System.out.println("Invalid suit");
+	}
+	
+	public String toString(){
+
+		return ("Attempted to create card with invalid suit argument" + " " + this.suitIdentifier);
+
+	}
+	
+	public char getSuitDesignator() {
+		
+		return suitIdentifier;
+	}
+}
---
+++ InvalidDeckPositionException.java
@@ -0,0 +1,25 @@
+public class InvalidDeckPositionException extends Exception {
+
+	private int positionIdentifier = 0;
+
+	public InvalidDeckPositionException(int inValidPosition) {
+
+		positionIdentifier = inValidPosition;
+
+		System.out.println("Invalid Position" + inValidPosition);
+
+	}
+
+	private InvalidDeckPositionException() {
+		System.out.println("Invalid Position");
+	}
+
+	public String toString() {
+
+		return ("Attempted to get a card from a position not in Deck" + " " + this.positionIdentifier);
+	}
+
+	public int getPositionValue() {
+		return positionIdentifier;
+	}
+}
